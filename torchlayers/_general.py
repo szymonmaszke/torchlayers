e9a8c2aa85f3ede51e277511323684f6c7662c95
vyzyv <vyz@protonmail.com>
2020-03-03 22:05:12 +0100

    Add concatenation layer

--- torchlayers/_general.py
+++ torchlayers/_general.py
@@ -29,3 +29,42 @@ class Lambda(torch.nn.Module):
 
     def forward(self, *args, **kwargs) -> typing.Any:
         return self.function(*args, **kwargs)
+
+
+class Concatenate(torch.nn.Module):
+    """Concatenate list of tensors.
+
+    Mainly useful in `torch.nn.Sequential` when previous layer returns multiple
+    tensors, e.g.:
+
+
+        class Foo(torch.nn.Module):
+            # Return same tensor three times
+            # You could explicitly return a list or tuple as well
+            def forward(tensor):
+                return tensor, tensor, tensor
+
+
+        model = torch.nn.Sequential(Foo(), torchlayers.Concatenate())
+        model(torch.randn(10, 20))
+
+    All tensors must have the same shape (except in the concatenating dimension).
+
+    Parameters
+    ----------
+    dim: int
+        Dimension along which tensors will be concatenated
+
+    Returns
+    -------
+    torch.Tensor
+        Concatenated tensor
+
+    """
+
+    def __init__(self, dim: int):
+        super().__init__()
+        self.dim: int = dim
+
+    def forward(self, tensor):
+        return torch.cat(tensor, dim=self.dim)
