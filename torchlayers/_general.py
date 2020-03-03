c25062dfde9996339a95d92b432e941b9007b7a5
vyzyv <vyz@protonmail.com>
2020-03-03 21:41:31 +0100

    Remove flatten layers

--- torchlayers/_general.py
+++ torchlayers/_general.py
@@ -8,18 +8,18 @@ from ._dev_utils.infer import create_forward, create_repr
 class Lambda(torch.nn.Module):
     """Use any function as `torch.nn.Module`
 
-    Simple proxy which allows you to use your own custom functions within
+    Simple proxy which allows you to use your own custom in
     `torch.nn.Sequential` and other requiring `torch.nn.Module` as input.
 
     Parameters
     ----------
-    function: typing.Callable
-            Any argument user specified function
+    function: Callable
+        Any user specified function
 
     Returns
     -------
-    typing.Any
-            Anything `function` returns
+    Any
+        Anything `function` returns
 
     """
 
@@ -29,17 +29,3 @@ class Lambda(torch.nn.Module):
 
     def forward(self, *args, **kwargs) -> typing.Any:
         return self.function(*args, **kwargs)
-
-
-class Flatten(torch.nn.Module):
-    """Flatten input (except batch dimension).
-
-    Returns
-    -------
-    torch.Tensor
-            Flattened input `torch.Tensor`.
-
-    """
-
-    def forward(self, inputs):
-        return inputs.reshape(inputs.shape[0], -1)
