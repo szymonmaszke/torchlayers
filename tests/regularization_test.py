d4b5579d1ca511dbae3579aa78579df2ca57891f
vyzyv <vyz@protonmail.com>
2020-05-21 15:36:40 +0200

    Add L1 & L2 regularization

---
+++ tests/regularization_test.py
@@ -0,0 +1,47 @@
+import itertools
+
+import torch
+
+import pytest
+import torchlayers as tl
+
+
+def single_module(regularization: str, name: str):
+    return getattr(tl, regularization)(tl.Linear(10), weight_decay=1000000, name=name)
+
+
+def multiple_modules(regularization: str, name: str):
+    return getattr(tl, regularization)(
+        torch.nn.Sequential(
+            tl.Linear(40), tl.ReLU(), tl.Linear(20), tl.ReLU(), tl.Linear(10)
+        ),
+        weight_decay=1000000,
+        name=name,
+    )
+
+
+def generate_inputs():
+    modules = (single_module, multiple_modules)
+    names = ("bias", "weight", None)
+    regularizations = ("L1", "L2")
+    steps = (1, 3)
+    for module, regularization, name, steps in itertools.product(
+        modules, regularizations, names, steps
+    ):
+        yield module(regularization, name), name, steps
+
+
+@pytest.mark.parametrize("module,name,steps", list(generate_inputs()))
+def test_weight_decay(module, name, steps: bool):
+    tl.build(module, torch.randn(1, 20))
+    for _ in range(steps):
+        output = module(torch.randn(5, 20))
+        output.sum().backward()
+    for param_name, param in module.named_parameters():
+        if name is not None:
+            if name in param_name:
+                assert torch.abs(param.grad).sum() > 10000.0
+            else:
+                assert torch.abs(param.grad).sum() < 10000.0
+        else:
+            assert torch.abs(param.grad).sum() > 10000.0
--- torchlayers/regularization.py
+++ torchlayers/regularization.py
@@ -1,3 +1,5 @@
+import abc
+
 import torch
 
 from . import module
@@ -28,10 +30,10 @@ class StochasticDepth(torch.nn.Module):
 
     Parameters
     ----------
-    module: torch.nn.Module
+    module : torch.nn.Module
         Any module whose output might be skipped
         (output shape of it has to be equal to the shape of inputs).
-    p: float, optional
+    p : float, optional
         Probability of survival (e.g. the layer will be kept). Default: ``0.5``
 
     """
@@ -64,9 +66,9 @@ class Dropout(module.InferDimension):
 
     Parameters
     ----------
-    p: float, optional
+    p : float, optional
         Probability of an element to be zeroed. Default: ``0.5``
-    inplace: bool, optional
+    inplace : bool, optional
         If ``True``, will do this operation in-place. Default: ``False``
 
     """
@@ -81,10 +83,6 @@ class Dropout(module.InferDimension):
             p=p,
             inplace=inplace,
         )
-
-    # Dropout can have any input shape according to documentation
-    def _module_not_found(self, inputs):
-        return torch.nn.Dropout
 
 
 class StandardNormalNoise(torch.nn.Module):
@@ -147,3 +145,106 @@ class UniformNoise(torch.nn.Module):
         if self.training:
             return inputs + torch.rand_like(inputs)
         return inputs
+
+
+class WeightDecay(torch.nn.Module):
+    def __init__(self, module, weight_decay, name: str = None):
+        if weight_decay <= 0.0:
+            raise ValueError(
+                "Regularization's weight_decay should be greater than 0.0, got {}".format(
+                    weight_decay
+                )
+            )
+
+        super().__init__()
+        self.module = module
+        self.weight_decay = weight_decay
+        self.name = name
+
+        self.hook = self.module.register_backward_hook(self._weight_decay_hook)
+
+    def remove(self):
+        self.hook.remove()
+
+    def _weight_decay_hook(self, *_):
+        if self.name is None:
+            for param in self.module.parameters():
+                if param.grad is None:
+                    param.grad = self.regularize(param)
+        else:
+            for name, param in self.module.named_parameters():
+                if self.name in name and param.grad is None:
+                    param.grad = self.regularize(param)
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
+
+    def extra_repr(self) -> str:
+        representation = "weight_decay={}".format(self.weight_decay)
+        if self.name is not None:
+            representation += ", name={}".format(self.name)
+        return representation
+
+    @abc.abstractmethod
+    def regularize(self, parameter):
+        pass
+
+
+class L2(WeightDecay):
+    r"""Regularize module's parameters using L2 weight decay.
+
+    Example::
+
+        import torchlayers as tl
+
+        # Regularize only weights of Linear module
+        regularized_layer = tl.L2(tl.Linear(30), weight_decay=1e-5, name="weight")
+
+    .. note::
+            Backward hook will be registered on `module`. If you wish
+            to remove `L2` regularization use `remove()` method.
+
+    Parameters
+    ----------
+    module : torch.nn.Module
+        Module whose parameters will be regularized.
+    weight_decay : float
+        Strength of regularization (has to be greater than `0.0`).
+    name : str, optional
+        Name of parameter to be regularized (if any).
+        Default: all parameters will be regularized (including "bias").
+
+    """
+
+    def regularize(self, parameter):
+        return self.weight_decay * parameter.data
+
+
+class L1(WeightDecay):
+    """Regularize module's parameters using L1 weight decay.
+
+    Example::
+
+        import torchlayers as tl
+
+        # Regularize all parameters of Linear module
+        regularized_layer = tl.L1(tl.Linear(30), weight_decay=1e-5)
+
+    .. note::
+            Backward hook will be registered on `module`. If you wish
+            to remove `L1` regularization use `remove()` method.
+
+    Parameters
+    ----------
+    module : torch.nn.Module
+        Module whose parameters will be regularized.
+    weight_decay : float
+        Strength of regularization (has to be greater than `0.0`).
+    name : str, optional
+        Name of parameter to be regularized (if any).
+        Default: all parameters will be regularized (including "bias").
+
+    """
+
+    def regularize(self, parameter):
+        return self.weight_decay * torch.sign(parameter.data)
