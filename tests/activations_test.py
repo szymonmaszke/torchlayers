e2cb4893df9b58bbb2ef2bf58d59feb0a4923a0d
vyzyv <vyz@protonmail.com>
2020-05-22 10:47:21 +0200

    Parametrize activations smoke tests

--- tests/activations_test.py
+++ tests/activations_test.py
@@ -1,7 +1,14 @@
 import torch
 
-import torchlayers
+import pytest
+import torchlayers as tl
 
 
-def test_hardsigmoid():
-    torchlayers.HardSigmoid()(torch.randn(4, 5, 6))
+@pytest.mark.parametrize("klass", ("Swish", "HardSwish", "HardSigmoid"))
+def test_object(klass):
+    getattr(tl, klass)()(torch.randn(4, 5, 6))
+
+
+@pytest.mark.parametrize("function", ("swish", "hard_swish", "hard_sigmoid"))
+def test_functional(function):
+    getattr(tl, function)(torch.randn(4, 5, 6))
